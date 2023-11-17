e57261b1df7805f99fbfe1272f5bb4aa4886b145
LeanderVanBaekel <leander.van.baekel@gmail.com>
2023-11-17 10:50:45 +0100

    Added Dutch translations

---
+++ resources/lang/nl/user.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    "resource" => [
+        "id" => "ID",
+        "single" => "Gebruiker",
+        "email_verified_at" => "Email Geverifieerd",
+        "created_at" => "Aangemaakt Op",
+        "updated_at" => "Bijgewerkt Op",
+        "verified" => "Geverifieerd",
+        "unverified" => "Niet geverifieerd",
+        "name" => "Naam",
+        "email" => "Email",
+        "password" => "Wachtwoord",
+        "roles" => "Rollen",
+        "label" => "Gebruikers",
+        "title" => [
+            "create" => "Maak Gebruiker",
+            "edit" => "Bewerk Gebruiker",
+            "list" => "Gebruikers",
+            "home" => "Gebruikers"
+        ],
+    ]
+];
