063d79e87ce727c3d7f35f7925c4cf86d11f6b20
Fady Mondy <info@3x1.io>
2024-09-17 19:46:32 +0300

    fix ->isEnabled()

--- src/FilamentUsersPlugin.php
+++ src/FilamentUsersPlugin.php
@@ -18,10 +18,8 @@ class FilamentUsersPlugin implements Plugin
 
     public function register(Panel $panel): void
     {
-        if(class_exists(Module::class)){
-            if(\Nwidart\Modules\Facades\Module::find('FilamentUsers')?->isEnabled()){
-                $this->isActive = true;
-            }
+        if(class_exists(Module::class) && \Nwidart\Modules\Facades\Module::find('FilamentUsers')?->isEnabled()){
+            $this->isActive = true;
         }
         else {
             $this->isActive = true;
